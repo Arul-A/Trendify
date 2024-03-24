bda6df9744f9021f34975c8f7967900203cb0fcc
Arul-A <akarul2100@gmail.com>
2024-03-25 01:39:16 +0530

    toggle navigation

---
+++ index.js
@@ -0,0 +1,10 @@
+var sidenav = document.querySelector(".side-navbar")
+function showNavbar()
+{
+    sidenav.style.display = "block";
+}
+function hideNavbar()
+{
+    sidenav.style.display = "none"
+}
+
