f12fff02552bf500f2688e33dd0bce7682b0b998
Arul-A <akarul2100@gmail.com>
2024-03-25 07:36:32 +0530

    side navbar animation added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var sidenav = document.querySelector(".side-navbar")
 function showNavbar()
 {
-    sidenav.style.display = "block";
+    sidenav.style.left = "0";
+      
 }
 function hideNavbar()
 {
-    sidenav.style.display = "none"
+    sidenav.style.left = "-60%"
 }
 
