93a381139e85510543292fa4c27cbb6217e557c3
Arul-A <akarul2100@gmail.com>
2024-04-25 15:22:24 +0530

    index page completed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var sidenav = document.querySelector(".side-navbar")
 function showNavbar()
 {
-    sidenav.style.left = "0";
-      
+    sidenav.style.left = "0"
 }
 function hideNavbar()
 {
     sidenav.style.left = "-60%"
 }
+
 
